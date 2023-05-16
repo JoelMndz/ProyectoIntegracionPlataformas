62c39214ed5d8e20a8a390b14bfbc9958722b0b2
unknown <luisjo3lml@gmail.com>
2023-05-16 10:51:39 -0500

    Configuracion de kubernetes,prometheus y grafana

--- api_banca/src/config/index.ts
+++ api_banca/src/config/index.ts
@@ -4,7 +4,7 @@ if(process.env.NODE_ENV !== 'production'){
   config();
 }
 
-export const PORT = process.env.PORT;
+export const PORT = process.env.PORT || 5002;
 export const MONGO_URI = process.env.MONGO_URI;
 export const SECRET = process.env.SECRET;
 export const EMAIL_USER = process.env.EMAIL_USER;
