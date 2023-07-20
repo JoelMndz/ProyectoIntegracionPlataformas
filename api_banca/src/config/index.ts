03cb5e2aa4b1b0b85c56d18c85414f2fdf479154
Dayanara Burgasi <dburgasi06@gmail.com>
2023-07-20 13:26:49 -0500

    Endpoint movimientos

--- api_banca/src/config/index.ts
+++ api_banca/src/config/index.ts
@@ -5,7 +5,9 @@ if(process.env.NODE_ENV !== 'production'){
 }
 
 export const PORT = process.env.PORT || 5002;
-export const MONGO_URI = process.env.MONGO_URI;
+export const MONGO_URI = 'mongodb+srv://admin:admin@cluster0.9p5wrdm.mongodb.net/api_banca_db?retryWrites=true&w=majority';
+// export const MONGO_URI = process.env.MONGO_URI;
+
 export const SECRET = process.env.SECRET;
-export const EMAIL_USER = process.env.EMAIL_USER;
-export const EMAIL_PASSWORD = process.env.EMAIL_PASSWORD;
+// export const EMAIL_USER = process.env.EMAIL_USER;
+// export const EMAIL_PASSWORD = process.env.EMAIL_PASSWORD;
--- api_banca/src/services/cuenta.ts
+++ api_banca/src/services/cuenta.ts
@@ -6,6 +6,14 @@ interface IRequestCrear{
   usuarioId: string,
 }
 
+interface IFiltrosMovimientos {
+  usuarioId?: string;
+  tipo?: string;
+  numero?:string;
+  fechaInicio?: string;
+  fechaFin?: string;
+}
+
 export const CuentaService = {
   crear: async(entidad:IRequestCrear)=>{
     
@@ -24,5 +32,39 @@ export const CuentaService = {
   listarCuenta: async(usuarioId:string)=>{
     const cuenta = await CuentaModel.find({usuarioId});
     return cuenta;
-}
+  },
+  obtenerMovimientos: async(filtros: IFiltrosMovimientos)=>{
+    try {
+      if(filtros){
+        const movimientos = await CuentaService.consultarMovimientos(filtros);
+        return movimientos;
+      }
+    } catch (error) {
+      throw new Error('Error al obtener los movimientos');
+    }
+  },
+  consultarMovimientos: async(filtros: IFiltrosMovimientos)=> {
+    const { usuarioId, tipo, numero, fechaInicio, fechaFin } = filtros;
+
+    const filters: any = {};
+
+    if (usuarioId) {
+      filters.usuarioId = usuarioId;
+    }
+
+    if (numero) {
+      filters.numero = numero;
+    }
+
+    if (tipo) {
+      filters['movimientos.tipo'] = tipo;
+    }
+
+    if (fechaInicio && fechaFin) {
+      filters['movimientos.fecha'] = { $gte: new Date(fechaInicio), $lte: new Date(fechaFin) };
+    }
+
+    const movimientos = await CuentaModel.find(filters).populate('movimientos');
+    return movimientos;
+  }
 };
