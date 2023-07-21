0fa34c357076f4b3ce57ceefae262fc349b08a48
Dayanara Burgasi <dburgasi06@gmail.com>
2023-07-20 23:32:07 -0500

    Obtener-movimientos-graphql

--- api_graphql/src/app.module.ts
+++ api_graphql/src/app.module.ts
@@ -1,10 +1,35 @@
+// import { Module } from '@nestjs/common';
+// import { AppController } from './app.controller';
+// import { AppService } from './app.service';
+
+// @Module({
+//   imports: [],
+//   controllers: [AppController],
+//   providers: [AppService],
+// })
+// export class AppModule {}
+
+// app.module.ts
+import { join } from 'path';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
+import { CuentasModule } from './movimientos/cuentas.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      playground: false,
+      autoSchemaFile: join(process.cwd(),'src/schema.gql'),
+      plugins:[
+        ApolloServerPluginLandingPageLocalDefault()
+      ]
+    }),
+    CuentasModule,
+  ],
 })
 export class AppModule {}
